cde91e7fb2d4db334f03eeebd2c42ca780fac38a
Christian Blavier <cblavier@gmail.com>
2021-03-26 18:16:22 +0100

    removed Code.ensure_compile/1 check

--- lib/phx_component_helpers.ex
+++ lib/phx_component_helpers.ex
@@ -216,14 +216,8 @@ defmodule PhxComponentHelpers do
   defp escaped(val, opts \\ [])
 
   defp escaped(val, json: true) do
-    case Code.ensure_compiled(@json_library) do
-      {:module, mod} ->
-        {:safe, escaped_val} = val |> mod.encode!() |> html_escape()
-        "\"#{escaped_val}\""
-
-      _ ->
-        raise ArgumentError, "#{@json_library} is not available"
-    end
+    {:safe, escaped_val} = val |> @json_library.encode!() |> html_escape()
+    "\"#{escaped_val}\""
   end
 
   defp escaped(val, _) do
